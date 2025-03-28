cf9f666318882a66ce068c2f0a54516534ca4606
hugo <smithdotng@gamil.com>
2025-03-28 09:20:37 +0100

    pwa shed.ng 10

--- index.js
+++ index.js
@@ -1629,7 +1629,7 @@ app.get('/handle-link', isAuthenticated, (req, res) => {
 });
 
 // Serve manifest.json
-app.get('/site.webmanifest', (req, res) => {
+app.get('/manifest.json', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'manifest.json'));
 });
 
